6df02b81cf953925e90a67f4e154303c75a8a993
Ernesto Mendoza Blanco <ernestom@menta.com.mx>
2018-12-18 08:29:43 +0000

    New Relic APM, Browser & Infrastructure instrumentation.

--- source/app.js
+++ source/app.js
@@ -1,3 +1,4 @@
+const newrelic = require('newrelic');
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -22,6 +23,24 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+// K8s metadata as custom attributes
+var CUSTOM_PARAMETERS = {
+  'K8S_NODE_NAME': process.env.K8S_NODE_NAME,
+  'K8S_HOST_IP': process.env.K8S_HOST_IP,
+  'K8S_POD_NAME': process.env.K8S_POD_NAME,
+  'K8S_POD_NAMESPACE': process.env.K8S_POD_NAMESPACE,
+  'K8S_POD_IP': process.env.K8S_POD_IP,
+  'K8S_POD_SERVICE_ACCOUNT': process.env.K8S_POD_SERVICE_ACCOUNT,
+  'K8S_POD_TIER': process.env.K8S_POD_TIER
+};
+app.use(function(req, res, next) {
+  newrelic.addCustomParameters(CUSTOM_PARAMETERS);
+  next();
+});
+
+// For Browser
+app.locals.newrelic = newrelic;
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
@@ -29,6 +48,7 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
+  newrelic.noticeError(err, CUSTOM_PARAMETERS);
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -37,5 +57,6 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
 
 module.exports = app;
